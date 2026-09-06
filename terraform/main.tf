2a98748109428f2dcc0b8917083a8aaf7998c977
RajpootDeepak <161629677+RajputDeepak69@users.noreply.github.com>
2026-09-06 15:01:27 +0530

    refactoring the terraform infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "site" {
   bucket         = var.site_bucket_name
   force_destroy  = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,29 +10,13 @@ variable "region" {
 
 variable "site_bucket_name" {
   type    = string
-  default = "my-portfolio-29"
-}
-
-variable "domain_name" {
-  type        = string
-  description = "Optional: full domain (eg. www.example.com). Leave empty to skip Route53 & ACM."
-  default     = ""
-}
-
-variable "hosted_zone_id" {
-  type        = string
-  description = "Required if domain_name is set."
-  default     = ""
 }
 
 variable "sender_email" {
   type        = string
-  description = "SES verified sender (if not using domain identity)."
-  default     = "100rajpootking100@gmail.com"
+  description = "SES verified sender"
 }
 
 variable "recipient_email" {
   type        = string
-  description = "Recipient of contact form messages."
-  default     = "feb29deepak@gmail.com"
 }
